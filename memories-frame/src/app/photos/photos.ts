a2347c630233b4b1d9465a148a52d71d606e7384
Steven Masfaraud <steven@masfaraud.fr>
2026-08-12 17:47:37 +0200

    ci: delete angular tests

--- memories-frame/src/app/photos/photos.ts
+++ memories-frame/src/app/photos/photos.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { Photo } from '../models/photos.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-photos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './photos.html',
   styleUrl: './photos.scss',
 })
 export class Photos {
 
-  photos: Photo[];
+  photos: Photo[] = [];
 
 }
